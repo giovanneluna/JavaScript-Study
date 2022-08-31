6a8800e998c4b23a0b103a4b37e2fce0e85d52d4
giovanneluna <gikalunalticg@gmail.com>
2022-08-31 14:53:56 -0300

    Study JS

---
+++ app.js
@@ -0,0 +1,14 @@
+alert('Trolei Você $%&@$%@%$@%$%@$%@$%@$%')
+
+function puxar(){
+    window.location.href = 'https://google.com'
+    console.log('ola')
+}
+
+function discord () {
+    window.location.href = 'https://discord.com/'
+}
+
+function kabum () {
+    window.location.href = 'https://www.kabum.com.br/'
+}
